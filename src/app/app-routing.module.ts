1f6532da07e46ac5eaa3d34cbb5eed374de90612
ingridhamorales <ingrihamoralesmail.com>
2022-11-02 19:55:01 -0500

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
