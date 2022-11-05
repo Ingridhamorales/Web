bf4e4b3fa622a2f6b4bcb9c88295163e948fa36c
ingridhamorales <ingrihamoralesmail.com>
2022-11-04 20:51:22 -0500

    tercer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,41 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: '/index'
+    
   },
+  {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },{
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },{
+    path: 'aeropuertos',
+    loadChildren: () => import('./modulos/aeropuertos/aeropuertos.module').then(m => m.AeropuertosModule)
+  },
+  {
+    path: 'rutas',
+    loadChildren: () => import('./modulos/rutas/rutas.module').then(m => m.RutasModule)
+  },{
+    path: 'vuelos',
+    loadChildren: () => import('./modulos/vuelos/vuelos.module').then(m => m.VuelosModule)
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/error'
+  }
 
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
