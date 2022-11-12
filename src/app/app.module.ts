5c11a2df52a2142cd1149e90264a1bd7bce87d16
ingridhamorales <ingrihamoralesmail.com>
2022-11-11 20:22:35 -0500

    cuarto commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './assets/navbar/navbar.component';
 import { FooterComponent } from './assets/footer/footer.component';
 import { IndexComponent } from './assets/index/index.component';
 import { ErrorComponent } from './assets/error/error.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { ErrorComponent } from './assets/error/error.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modelos/vuelo.model.ts
@@ -0,0 +1,9 @@
+export class VueloModel{
+    id?: String;
+    fecha_inicio?: String;
+    hora_inicio?: string;
+    fecha_fin?: string;
+    hora_fin?: string;
+    asientos_vendidos?: string;
+    nombre_piloto?: string;
+  }
